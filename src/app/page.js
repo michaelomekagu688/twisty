6fdf2d5d38d12a962c424307d1089f4e733979b8
Omekagu Michael <michaelomekagu@gmail.com>
2025-08-27 12:55:38 +0100

    whitecard temporary finished

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,15 @@ import Navbar from "@/components/navbar/Navbar";
 export default function Home() {
   return (
    <div className="container"> 
-   <Navbar />
-    <WhiteCard />
+    <Navbar />
+    <div className="inner-cont">
+      <div className="left">
+        <WhiteCard />
+      </div>
+      <div className="right">
+
+      </div>
+    </div>
    </div>
   );
 }
--- src/components/whitecard/WhiteCard.jsx
+++ src/components/whitecard/WhiteCard.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import './whitecard.scss'
+import { CreditCard, KeyboardArrowDownOutlined } from '@mui/icons-material'
 
 const WhiteCard = () => {
   return (
     <div className='whitecard'>
-        <h1>  </h1>
+      <div className='top'>
+        <CreditCard className='credit-icon'/>
+        <span className='logo-span'> Income Tracker </span>
+        <div className='pop-down'>
+          <div className='week'> Week </div>
+          <KeyboardArrowDownOutlined className='keyboard-icon' />
+        </div>
+      </div>
+      <div className='mid'>
+        <p className='mid-para'>
+          Track changes in income overtime and access detailed data on each project and payments received
+        </p>
+      </div>
+      <div className='bot'>
+        <div className='bot-left'>
+          <div className='bot-left-top'> +20%  </div>
+          <div className='bot-left-bottom'> This week's income is higher than last week's </div>
+        </div>
+        <div className='bot-right'>
+          <div className='graph'> Graph </div>
+        </div>
+      </div>
     </div>
   )
 }
